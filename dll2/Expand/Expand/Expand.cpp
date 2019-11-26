b99ab1ae270f69594730587c6b5b2070f6101173
D01nb <55731543+D01nb@users.noreply.github.com>
2019-11-26 18:12:20 +0800

    动态库实现与显式调用

---
+++ dll2/Expand/Expand/Expand.cpp
@@ -0,0 +1,93 @@
+﻿// MathClient.cpp : Client app for MathLibrary DLL.
+// #include "pch.h" Uncomment for Visual Studio 2017 and earlier
+#include <iostream>
+#include"windows.h"
+
+using namespace std;
+typedef void(*PRINT)(const unsigned long long a, const unsigned long long b);
+
+static unsigned long long previous_;  // Previous value, if any
+static unsigned long long current_;   // Current sequence value
+static unsigned index_;               // Current seq. position
+HMODULE hDLL = LoadLibrary(L"../../ Mathlibrary/Debug/Mathlibrary.dll");
+
+
+
+
+// Initialize a Fibonacci relation sequence
+// such that F(0) = a, F(1) = b.
+// This function must be called before any other function.
+void fibonacci_init(
+	const unsigned long long a,
+	const unsigned long long b)
+{
+	index_ = 0;
+	current_ = a;
+	previous_ = b; // see special case when initialized
+}
+
+// Produce the next value in the sequence.
+// Returns true on success, false on overflow.
+bool fibonacci_next()
+{
+	// check to see if we'd overflow result or position
+	if ((ULLONG_MAX - previous_ < current_) ||
+		(UINT_MAX == index_))
+	{
+		return false;
+	}
+
+	// Special case when index == 0, just return b value
+	if (index_ > 0)
+	{
+		// otherwise, calculate next sequence value
+		previous_ += current_;
+	}
+	std::swap(current_, previous_);
+	++index_;
+	return true;
+}
+
+// Get the current value in the sequence.
+unsigned long long fibonacci_current()
+{
+	return current_;
+}
+
+// Get the current index position in the sequence.
+unsigned fibonacci_index()
+{
+	return index_;
+}
+
+
+
+
+
+
+
+int main()
+{
+
+
+	
+	HMODULE hDLL = LoadLibrary(L"../../ Mathlibrary/Debug/Mathlibrary.dll");
+
+		
+		
+	
+    
+    fibonacci_init(1,1);
+    
+
+    do {
+        std::cout << fibonacci_index() << ": "
+            << fibonacci_current() << std::endl;
+    } while (fibonacci_next());
+    // Report count of values written before overflow.
+    std::cout << fibonacci_index() + 1 <<
+        " Fibonacci sequence values fit in an " <<
+        "unsigned 64-bit integer." << std::endl;
+	FreeLibrary(hDLL);
+}
+
